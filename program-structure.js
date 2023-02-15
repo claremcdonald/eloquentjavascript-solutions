ba253c345e4f0169463f0c4fcefb2cb6505b283f
ClareMcDonald <clare.s.mcdonald@gmail.com>
2023-02-15 13:20:51 -0800

    solved FizzBuzz

--- program-structure.js
+++ program-structure.js
@@ -2,3 +2,16 @@
 for (let result = '#'; result.length < 8; result += '#') {
     console.log(result);
 }
+
+// FizzBuzz
+for (i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
