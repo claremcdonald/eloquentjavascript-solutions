8225308c95b85c181638dec4dd8b72a9fe451c02
ClareMcDonald <clare.s.mcdonald@gmail.com>
2023-03-03 15:52:24 -0800

    solved Recursion

--- functions.js
+++ functions.js
@@ -8,3 +8,22 @@ function min(x, y) {
 }
 
 console.log(min(5, 6));
+console.log(min(9, -1));
+
+// Recursion
+function isEven(n) {
+    if (n === 0) {
+        return true;
+    } else if (n === 1) {
+        return false;
+    } else if (n < 0) {
+        return isEven(-n);
+    } else {
+        return isEven(n - 2);
+    }
+}
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-6));
+console.log(isEven(-7));
