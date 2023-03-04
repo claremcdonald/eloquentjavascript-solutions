898cdc01a671a5fb81f39dbaadb1ee4249e9a91a
ClareMcDonald <clare.s.mcdonald@gmail.com>
2023-03-03 16:35:20 -0800

    solved Bean Counting:

--- functions.js
+++ functions.js
@@ -19,6 +19,7 @@ function isEven(n) {
     } else if (n < 0) {
         return isEven(-n);
     } else {
+        // I want to go back to this step for better comprehension.
         return isEven(n - 2);
     }
 }
@@ -27,3 +28,39 @@ console.log(isEven(75));
 console.log(isEven(-1));
 console.log(isEven(-6));
 console.log(isEven(-7));
+
+// Bean Counting
+
+// first step
+// function countBs(string) {
+//     let count = 0;
+//     for (let x = 0; x < string.length - 1; x++) {
+//         if (string[x] === 'B') {
+//             count += 1;
+//         } else {
+//             count;
+//         }
+//     }
+//     return count;
+// }
+
+// second step
+function countBs(string) {
+    return countChar(string, 'B');
+}
+
+function countChar(string, char) {
+    let count = 0;
+    // why not string.length - 1?
+    for (let x = 0; x < string.length; x++) {
+        if (string[x] === char) {
+            count += 1;
+            //else statement is unnecessary
+        } else {
+            count;
+        }
+    }
+    return count;
+}
+
+console.log(countBs('BaBBoboo', 'B'));
